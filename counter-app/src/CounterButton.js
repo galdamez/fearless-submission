2ef5e4270a4936cdfb99988f37e736e5cbe0d7fa
Jose Galdamez <jose.g.galdamez@nasa.gov>
2020-12-18 22:54:26 -0500

    Add loading placeholder

--- counter-app/src/CounterButton.js
+++ counter-app/src/CounterButton.js
@@ -2,12 +2,15 @@ import React, { useState } from 'react';
 
 export default function CounterButton() {
   const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(false);
 
-   async function fetchCount() {
+  async function fetchCount() {
     const url = 'https://api.countapi.xyz/hit/fearless-counter/1ccb732e-b55a-4404-ad3f-0f99c02fe44e';
+    setLoading(true);
     const response = await fetch(url);
     const data = await response.json();
     setCount(data.value);
+    setLoading(false);
   }
 
   return (
@@ -18,7 +21,12 @@ export default function CounterButton() {
       {
         count > 0 && 
         <div className="count-container">
-          {count} hits
+          {
+            loading ? 
+              <span className="loading-message">Loading&hellip;</span>
+              :
+              <span>{count} hits</span>
+          }
         </div>
       }
     </>
