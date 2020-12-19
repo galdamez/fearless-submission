7e4e1e1ee2b787f499fe5c8e73a038cd88329e54
Jose Galdamez <jose.g.galdamez@nasa.gov>
2020-12-19 00:05:21 -0500

    Refactor loading state name

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -6,11 +6,11 @@ import { fetchCount } from './services/fetchService';
 
 export default function App() {
   const [count, setCount] = useState(0);
-  const [loading, setLoading] = useState(false);
+  const [loading, setCountLoading] = useState(false);
 
   return (
     <div className="app-container">
-      <CounterButton clickHandler={() => fetchCount(setCount, setLoading)} />
+      <CounterButton clickHandler={() => fetchCount(setCount, setCountLoading)} />
       <LoadingMessage count={count} loading={loading} />
     </div>
   );
--- counter-app/src/services/fetchService.js
+++ counter-app/src/services/fetchService.js
@@ -1,8 +1,8 @@
-export async function fetchCount(setCount, setLoading) {
+export async function fetchCount(setCount, setCountLoading) {
   const url = 'https://api.countapi.xyz/hit/fearless-counter/1ccb732e-b55a-4404-ad3f-0f99c02fe44e';
-  setLoading(true);
+  setCountLoading(true);
   const response = await fetch(url);
   const data = await response.json();
   setCount(data.value);
-  setLoading(false);
+  setCountLoading(false);
 }
